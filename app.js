96d2f959a7f3c1c039aaccfd89152ca00950ece6
DaoNgocHaiAnh <anhdnhph26652@fpt.edu.vn>
2024-03-22 11:20:37 +0700

    api_mission

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const missionRoutes = require('./routes/missionRoutes');
 
 var app = express();
+mongoose.connect('mongodb://localhost:27017/ThucTapDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch(err => {
+    console.log("Could not connect to MongoDB", err);
+    process.exit();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', missionRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controller/missionController.js
@@ -0,0 +1,72 @@
+const Mission = require('../models/mission')
+
+exports.createMission = (req, res) => {
+    // Kiểm tra xem tất cả các trường cần thiết đã được cung cấp hay không
+    if (!req.body.title || !req.body.point || !req.body.detail) {
+        return res.status(400).send("Missing required fields");
+    }
+
+    const newMission = new Mission({
+        title: req.body.title,
+        point: req.body.point,
+        completed: req.body.completed || false,
+        detail: req.body.detail
+    });
+
+    newMission.save()
+        .then(mission => {
+            res.json(mission);
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save mission to database");
+        });
+};
+
+exports.getAllMissions = (req, res) => {
+    Mission.find()
+        .then(missions => {
+            res.json(missions);
+        })
+        .catch(err => {
+            res.status(400).send("Unable to fetch missions");
+        });
+};
+
+exports.getMissionById = (req, res) => {
+    Mission.findById(req.params.id)
+        .then(mission => {
+            if (!mission) {
+                return res.status(404).send("Mission not found");
+            }
+            res.json(mission);
+        })
+        .catch(err => {
+            res.status(400).send("Unable to fetch mission");
+        });
+};
+
+exports.updateMission = (req, res) => {
+    Mission.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(mission => {
+            if (!mission) {
+                return res.status(404).send("Mission not found");
+            }
+            res.json(mission);
+        })
+        .catch(err => {
+            res.status(400).send("Unable to update mission");
+        });
+};
+
+exports.deleteMission = (req, res) => {
+    Mission.findOneAndDelete({ _id: req.params.id })
+        .then(mission => {
+            if (!mission) {
+                return res.status(404).send("Mission not found");
+            }
+            res.json({ message: "Mission deleted successfully" });
+        })
+        .catch(err => {
+            res.status(400).send("Unable to delete mission");
+        });
+};
